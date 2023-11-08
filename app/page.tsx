3574d6d29f9053954919c0fe030a813558221d4e
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-08 23:56:59 +0600

    deleted tailwind.config.js and modify tailwind.config.ts

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>This is a landing page</h1>
+      <h1 className=" text-red-500 text-3xl font-bold">
+        This is a landing page
+      </h1>
     </div>
   );
 }
