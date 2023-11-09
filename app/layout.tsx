08a4e9044f4efe735c1c2688de30038be23b03fc
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-09 17:32:15 +0600

    created the Modal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <Modal />
           <Navbar />
         </ClientOnly>
         {children}
