dbea7619b5547e60e1dddde5f17f57cfd8edec77
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-11 02:05:00 +0600

    Fixing Modal.tsx css style

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -70,45 +70,47 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
-        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+      <div className="flex justify-center items-center w-full h-full fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full mt-[15%] md:mt-[5%]">
           {/* CONTENT */}
           <div
             className={`translate duration-300 h-full ${
               showModal ? "translate-y-0" : "translate-y-full"
             } ${showModal ? "opacity-100" : "opacity-0"}`}
           >
-            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white focus:outline-none">
+            <div className="relative translate h-[90%] border-0 rounded-xl shadow-lg flex flex-col w-full bg-white focus:outline-none">
               {/* MODAL HEADEAR */}
-              <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+              <div className="w-full flex items-center rounded-t-xl justify-center h-auto bg-white border-b-[1px] z-30">
                 <button
                   onClick={handleClose}
                   className="p-1 border-0 hover:opacity-70 transition absolute left-9"
                 >
                   <IoMdClose size={18} />
                 </button>
-                <div className="text-lg font-semibold">{title}</div>
+                <div className="text-lg font-bold p-3">{title}</div>
               </div>
-              {/* MODAL BODY */}
-              <div className="relative p-6 flex-auto">{body}</div>
-              {/* MODAL FOOTER */}
-              <div className="flex flex-col gap-2 p-6">
-                <div className="flex flex-row items-center gap-4 w-full">
-                  <Button
-                    disabled={disabled}
-                    label={actionLabel}
-                    onClick={handleSubmit}
-                  />
-                  {secondaryAction && secondaryActionLabel && (
+              <div className="overflow-y-scroll rounded-lg p-3">
+                {/* MODAL BODY */}
+                <div className="p-6 flex-auto">{body}</div>
+                {/* MODAL FOOTER */}
+                <div className="flex flex-col gap-2 p-6">
+                  <div className="flex flex-row items-center gap-4 w-full">
                     <Button
-                      outline
                       disabled={disabled}
-                      label={secondaryActionLabel}
-                      onClick={handleSecondaryAction}
+                      label={actionLabel}
+                      onClick={handleSubmit}
                     />
-                  )}
+                    {secondaryAction && secondaryActionLabel && (
+                      <Button
+                        outline
+                        disabled={disabled}
+                        label={secondaryActionLabel}
+                        onClick={handleSecondaryAction}
+                      />
+                    )}
+                  </div>
+                  {footer}
                 </div>
-                {footer}
               </div>
             </div>
           </div>
