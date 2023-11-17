45547253f18f503fecb31116bf71d93fecbfccf4
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-17 18:37:15 +0600

    Added outside clicking Modal closing fnc in Modal.tsx

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -64,13 +64,26 @@ const Modal: React.FC<ModalProps> = ({
     secondaryAction();
   }, [disabled, secondaryAction]);
 
+  const handleOverlayClick = useCallback(
+    (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      // Close the modal only if the click occurred outside the modal content
+      if (e.target === e.currentTarget) {
+        handleClose();
+      }
+    },
+    [handleClose]
+  );
+
   if (!isOpen) {
     return null;
   }
 
   return (
     <>
-      <div className="flex justify-center items-center w-full h-full fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+      <div
+        onClick={handleOverlayClick}
+        className="flex justify-center items-center w-full h-full fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70"
+      >
         <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full mt-[15%] md:mt-[5%]">
           {/* CONTENT */}
           <div
