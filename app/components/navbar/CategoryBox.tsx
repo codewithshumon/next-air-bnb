42d86bee7dd28a121452a2cadd5436d27509d759
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-24 01:52:41 +0600

    Adding QueryString Functing to Gategory Components

--- app/components/navbar/CategoryBox.tsx
+++ app/components/navbar/CategoryBox.tsx
@@ -1,4 +1,7 @@
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import queryString from "query-string";
 
 interface CategoryBoxProps {
   label: string;
@@ -13,8 +16,41 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   description,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    const updateQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updateQuery.category;
+    }
+
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: updateQuery,
+      },
+      ///{skipNull is for remove space or null from query}
+      { skipNull: true }
+    );
+
+    //redirecting to the URL with the updateQuery
+    router.push(url);
+  }, [label, params, router]);
+
   return (
     <div
+      onClick={handleClick}
       className={`
       flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer
       ${selected ? "border-b-neutral-800" : "border-transparent"}
