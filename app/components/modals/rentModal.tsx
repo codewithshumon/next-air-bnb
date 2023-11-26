b9118b1d7f84458412c28d1a6c56e9e1aaab0ea3
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-26 14:30:38 +0600

    Added Listing Creating UI form, and first setp Category select option. in RentModal

--- app/components/modals/rentModal.tsx
+++ app/components/modals/rentModal.tsx
@@ -1,10 +1,104 @@
 "use client";
 
+import { useMemo, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
 import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CagetoryInput from "../inputs/CategoryInput";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DISCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathroomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      discription: "",
+    },
+  });
+
+  const category = watch("category");
+
+  //react setValue hook does set the value but it does not rerender the page
+  //so we need to set a custome value
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+
+    return "Back";
+  }, [step]);
+
+  let bodyConetent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describes your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CagetoryInput
+              onClick={(category) => setCustomValue("category", category)}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   return (
     <div>
@@ -12,8 +106,11 @@ const RentModal = () => {
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
         onSubmit={rentModal.onClose}
-        actionLabel="Submit"
+        actionLabel={actionLabel}
+        secondaryActionLabel={secondaryActionLabel}
+        secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
         title="Airbnb your home!"
+        body={bodyConetent}
       />
     </div>
   );
