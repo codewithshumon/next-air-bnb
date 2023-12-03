6a8c93fe1ddb3c2abe4f0fa4ce7cac94e5e04a1c
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-12-03 17:37:52 +0600

    Added a Refresh tooltip to FavoritesClient

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { RiRefreshLine } from "react-icons/ri";
 
 import { Listing } from "@prisma/client";
 import { SafeUser } from "../types";
@@ -19,16 +20,28 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
   currentUser,
 }) => {
   const router = useRouter();
+
   return (
     <Container>
       <Heading
+        center
         title="Favorites"
         subtitle="List of places you have favorited!"
       />
-      <div
-        onClick={() => router.refresh()}
-        className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8"
-      >
+      <div className="mt-1 w-fit font-semibold flex flex-row gap-2">
+        <div className="group relative flex items-center cursor-pointer">
+          <RiRefreshLine
+            size={30}
+            className="text-gray-800"
+            onClick={() => router.refresh()}
+          />
+          <div className="absolute hidden group-hover:block w-[125px] bg-gray-800 text-white text-sm p-2 rounded-full left-[105%]">
+            Refresh favorites
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {listings.map((listing) => (
           <ListingCard
             key={listing.id}
